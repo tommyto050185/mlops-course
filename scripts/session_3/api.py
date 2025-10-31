6d62c08387161da2b7f7a254f00debd36148d077
tommyto050185 <thtphuong123@gmail.com>
2025-10-31 16:29:04 +0700

    upload

--- scripts/session_3/api.py
+++ scripts/session_3/api.py
@@ -7,5 +7,33 @@ app = FastAPI()
 @app.get(f"/")
 def root():
     return {"message": "Hello World from FastAPI!"}
+
+@app.get(f"/health")
+def health(num_input: int):
+    if num_input > 10:
+        return {"status": "healthy"}
+    else:
+        return {"status": "unhealthy"}
+
+class CalculateRequest(BaseModel):
+    method: str
+    num1: float
+    num2: float
+class CalculateResponse(BaseModel):
+    result: float
+
+
+@app.post(f"/calculate", response_model=CalculateResponse)
+def calculate(request: CalculateRequest)-> CalculateResponse:    
+    if  request.method == "add":
+        result = request.num1 + request.num2
+    elif request.method == "subtract":
+        result = request.num1 - request.num2
+    elif request.method == "multiply":
+        result = request.num1 * request.num2
+    elif request.method == "divide":
+        result = request.num1 / request.num2
+    return CalculateResponse(result=result)
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000) 
